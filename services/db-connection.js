a577f6ca66d1b690285609ea1b37915a02295d9e
Juan <jjrosalesuci@gmail.com>
2020-06-09 19:13:17 -0300

    Fixes

--- services/db-connection.js
+++ services/db-connection.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host:     '127.0.0.1',
-    user:     'root',
-    password: 'password',
-    database: 'senpai-todo-list'
+    host: '127.0.0.1',
+    user: 'root',
+    port:  8889,
+    password: 'root',
+    database: 'senpai'
 });
 
 connection.connect();
